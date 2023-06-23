6aba309be28b0cf983b181738b8c4bb8a0a545fa
rahulprasad0710 <raulshah2021@hotmail.com>
2023-06-23 15:27:24 +0545

    registartion page api

---
+++ src/types/payload.type.ts
@@ -0,0 +1,17 @@
+export interface IUserResponseResponse {
+    id: string;
+    first_name: string;
+    last_name: string;
+    mobile: string;
+    email: string;
+    status: string | null;
+    status_id: number;
+    secondary_email: string;
+    country_calling_code: string;
+    country: string;
+    state: string;
+    city: string;
+    street_address: string;
+    profile_picture: string;
+    postal_code: string;
+}
---
+++ src/ui/shared/atoms/CountryCode.tsx
@@ -0,0 +1,56 @@
+import Select from "react-select";
+import { useMemo } from "react";
+import COUNTRY_CALLING_CODE from "../../../constants/Country.json";
+import { ISelectProps } from "../../../types/react-select.type";
+
+interface ICountryCodeProps {
+    selectValue: ISelectProps | undefined;
+    setSelectValue: (value: ISelectProps | undefined) => void;
+    placeholder: string;
+    forCountry?: boolean;
+}
+
+interface ICountryProps {
+    country: string;
+    code: string;
+    iso: string;
+}
+
+const CountryCode = (props: ICountryCodeProps) => {
+    const { selectValue, setSelectValue, placeholder, forCountry } = props;
+
+    const options1 = useMemo(() => {
+        return COUNTRY_CALLING_CODE.map((countryItem: ICountryProps) => {
+            return {
+                label: `(${countryItem.code}) ${countryItem.country} - ${countryItem.iso}`,
+                value: countryItem.code,
+            };
+        });
+    }, []);
+
+    const options2 = useMemo(() => {
+        return COUNTRY_CALLING_CODE.map((countryItem: ICountryProps) => {
+            return {
+                label: countryItem.country,
+                value: countryItem.country,
+            };
+        });
+    }, []);
+
+    const handleChange = (selectedOption: ISelectProps | null) => {
+        if (selectedOption) {
+            setSelectValue(selectedOption);
+        }
+    };
+
+    return (
+        <Select
+            options={forCountry ? options2 : options1}
+            value={selectValue}
+            placeholder={placeholder}
+            onChange={handleChange}
+        />
+    );
+};
+
+export default CountryCode;
