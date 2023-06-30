35c6335ebaba8357e1b8ef5e45e36442a2279931
rahulprasad0710 <raulshah2021@hotmail.com>
2023-06-30 11:52:44 +0545

    -feat : upload features added

--- src/ui/features/user/Dashboard.tsx
+++ src/ui/features/user/Dashboard.tsx
@@ -1,25 +1,7 @@
-import UploadFile from "../../shared/components/Upload";
-import FILE_UPLOAD_TYPE from "../../../constants/FileUpload";
-import { useState } from "react";
-import FileList from "../../shared/components/FileList";
-
 const Dashboard = () => {
-    const [imageInfo, setImageInfo] = useState<string[] | undefined>();
     return (
         <div className='container'>
-            {imageInfo && imageInfo?.length > 0 && (
-                <FileList fileInfo={imageInfo} />
-            )}
-            <UploadFile
-                title='Upload File'
-                fileUploadType={FILE_UPLOAD_TYPE.JSON}
-                isMultiple={true}
-                isRoutePrivate={true}
-                isUploadRequired={true}
-                fileUploadLimit={4}
-                fileInfo={imageInfo}
-                setFileInfo={setImageInfo}
-            />
+            <h3>Dashboard</h3>
         </div>
     );
 };
