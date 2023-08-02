93841dfecf228e009a866aa0bc5cd72b64984800
rahulprasad0710 <raulshah2021@hotmail.com>
2023-08-02 12:09:09 +0545

    -feat:serverSelect

--- src/ui/features/crm/enrollmentOpening/Add.tsx
+++ src/ui/features/crm/enrollmentOpening/Add.tsx
@@ -13,7 +13,6 @@ import API_ROUTE from "../../../../service/api";
 import useMutation from "../../../../hooks/useMutation";
 import { Spinner } from "react-bootstrap";
 import useHandleShowError from "../../../../hooks/useHandleShowError";
-import CompanyBreadcrumb from "../../../shared/molecules/CompanyBreadcrumb";
 import { IVisaTypeResponse } from "../../../../types/payload.type";
 import SeverSelect from "../../../shared/components/ServerSelect";
 
@@ -35,10 +34,14 @@ const Add = () => {
         API_ROUTE.CM_ENROLLMENT,
         true
     );
-
-    const [selectedVisaType, setSelectedVisaType] = useState<
+    const showDataLabel: keyof IVisaTypeResponse = "name";
+    const [selectedListItem, setselectedListItem] = useState<
         IVisaTypeResponse | undefined
     >();
+
+    const [selectedItemText, setSelectedItemText] = useState<string>(
+        " medical Insureance"
+    );
 
     const {
         register,
@@ -105,155 +108,150 @@ const Add = () => {
         }
     });
     return (
-        <div>
-            <CompanyBreadcrumb
-                title={
-                    location?.state?.data?.id
-                        ? "CM Update Institute"
-                        : "Cm Add Institute"
-                }
-                showBreadcrumb={true}
-                btnText='Back'
-            />
-            <div className='card shadow-sm '>
-                <div className='card-header'>
-                    <h3 className='card-title'>
-                        {location?.state?.data?.id
-                            ? "Update Institute Details"
-                            : "Add Institute Details"}
-                    </h3>
-                    <div className='card-toolbar'>
-                        {location?.state?.data?.id ? (
-                            <button
-                                className='btn btn-sm btn-info'
-                                onClick={handleResetForm}>
-                                Reset
-                            </button>
-                        ) : (
-                            <button
-                                className='btn btn-sm btn-info'
-                                onClick={handleClearForm}>
-                                Clear
-                            </button>
-                        )}
-                    </div>
+        <div className='card shadow-sm '>
+            <div className='card-header'>
+                <h3 className='card-title'>
+                    {location?.state?.data?.id
+                        ? "Update Institute Details"
+                        : "Add Institute Details"}
+                </h3>
+                <div className='card-toolbar'>
+                    {location?.state?.data?.id ? (
+                        <button
+                            className='btn btn-sm btn-info'
+                            onClick={handleResetForm}>
+                            Reset
+                        </button>
+                    ) : (
+                        <button
+                            className='btn btn-sm btn-info'
+                            onClick={handleClearForm}>
+                            Clear
+                        </button>
+                    )}
                 </div>
-                <div className='card-body'>
-                    <form className='form w-100 ' onSubmit={onFormSubmit}>
-                        <div className='row'>
-                            <div className='col-6 gap-5 gap-md-7  mb-6'>
-                                <div className='fv-row flex-row-fluid fv-plugins-icon-container'>
-                                    <label className='required form-label'>
-                                        Select Visa Type
-                                    </label>
-                                    {/* <AsyncSelect
+            </div>
+            <div className='card-body'>
+                <form className='form w-100 ' onSubmit={onFormSubmit}>
+                    <div className='row'>
+                        <div className='col-6 gap-5 gap-md-7  mb-6'>
+                            <div className='fv-row flex-row-fluid fv-plugins-icon-container'>
+                                <label className='required form-label'>
+                                    Select Visa Type
+                                </label>
+                                {/* <AsyncSelect
                                         baseUrl={API_ROUTE.CM_AGENTS}
                                         placeholder='Visa Type'
                                         selectValue={selectedVisaType}
                                         setSelectValue={setSelectedVisaType}
                                         showData='name'
                                     /> */}
-                                    {/* <AsyncReactSelect
+                                {/* <AsyncReactSelect
                                         baseUrl={API_ROUTE.CM_AGENTS}
                                         placeholder='Visa Type'
                                         showData='name'
                                     /> */}
 
-                                    <SeverSelect
-                                        baseUrl={API_ROUTE.CM_AGENTS}
-                                    />
-                                </div>
-                            </div>
-                            <div className='col-6 gap-5 gap-md-7  mb-6'>
-                                <div className='fv-row flex-row-fluid fv-plugins-icon-container'>
-                                    <label className='required form-label'>
-                                        Opening Start Date
-                                    </label>
-                                    <input
-                                        type='date'
-                                        className='form-control'
-                                        {...register("enroll_start_date")}
-                                        placeholder='website url'
-                                    />
-                                    <div className='fv-plugins-message-container invalid-feedback'>
-                                        {errors.enroll_start_date?.message}
-                                    </div>
-                                </div>
-                            </div>
-                            <div className='col-6 gap-5 gap-md-7   mb-6'>
-                                <div>
-                                    <label className='required form-label'>
-                                        Opening End Date
-                                    </label>
-                                    <input
-                                        type='date'
-                                        className='form-control'
-                                        placeholder='Enter your state'
-                                        {...register("enroll_end_date")}
-                                    />
-                                    <div className='fv-plugins-message-container invalid-feedback'>
-                                        {errors.enroll_end_date?.message}
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div className='col-12 gap-5 gap-md-7 mb-6'>
-                                <div className='fv-row flex-row-fluid fv-plugins-icon-container'>
-                                    <label className='required form-label'>
-                                        Postion/Course
-                                    </label>
-                                    <input
-                                        className='form-control'
-                                        placeholder='College/University name'
-                                        {...register("position")}
-                                    />
-                                    <div className='fv-plugins-message-container invalid-feedback'>
-                                        {errors.position?.message}
-                                    </div>
-                                </div>
-                            </div>
-                            <div className='col-12 gap-5 gap-md-7  mb-6'>
-                                <div className='fv-row flex-row-fluid fv-plugins-icon-container'>
-                                    <label className=' form-label'>
-                                        Description
-                                    </label>
-                                    <textarea
-                                        className='form-control'
-                                        {...register("description")}
-                                        placeholder='description'
-                                    />
-                                    <div className='fv-plugins-message-container invalid-feedback'>
-                                        {errors.description?.message}
-                                    </div>
-                                </div>
-                            </div>
-                            <div className='col-12 d-flex justify-content-end'>
-                                <button
-                                    type='submit'
-                                    disabled={isLoading}
-                                    className='btn btn-primary  mb-6'>
-                                    {isLoading ? (
-                                        <>
-                                            <span className='ms-2'>
-                                                Please Wait...
-                                            </span>
-                                            <Spinner
-                                                size='sm'
-                                                animation='border'
-                                                role='status'></Spinner>
-                                        </>
-                                    ) : (
-                                        <span>
-                                            {location?.state?.data?.id
-                                                ? "Update"
-                                                : "Submit"}
-                                        </span>
-                                    )}
-                                </button>
+                                <SeverSelect
+                                    showDataLabel={showDataLabel}
+                                    selectedListItem={selectedListItem}
+                                    setselectedListItem={setselectedListItem}
+                                    selectedItemText={selectedItemText}
+                                    setSelectedItemText={setSelectedItemText}
+                                    placeholder='Search visa Type'
+                                    baseUrl={API_ROUTE.CM_AGENTS}
+                                />
                             </div>
                         </div>
-                    </form>
-                </div>
+                        <div className='col-6 gap-5 gap-md-7  mb-6'>
+                            <div className='fv-row flex-row-fluid fv-plugins-icon-container'>
+                                <label className='required form-label'>
+                                    Opening Start Date
+                                </label>
+                                <input
+                                    type='date'
+                                    className='form-control'
+                                    {...register("enroll_start_date")}
+                                    placeholder='website url'
+                                />
+                                <div className='fv-plugins-message-container invalid-feedback'>
+                                    {errors.enroll_start_date?.message}
+                                </div>
+                            </div>
+                        </div>
+                        <div className='col-6 gap-5 gap-md-7   mb-6'>
+                            <div>
+                                <label className='required form-label'>
+                                    Opening End Date
+                                </label>
+                                <input
+                                    type='date'
+                                    className='form-control'
+                                    placeholder='Enter your state'
+                                    {...register("enroll_end_date")}
+                                />
+                                <div className='fv-plugins-message-container invalid-feedback'>
+                                    {errors.enroll_end_date?.message}
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='col-12 gap-5 gap-md-7 mb-6'>
+                            <div className='fv-row flex-row-fluid fv-plugins-icon-container'>
+                                <label className='required form-label'>
+                                    Postion/Course
+                                </label>
+                                <input
+                                    className='form-control'
+                                    placeholder='College/University name'
+                                    {...register("position")}
+                                />
+                                <div className='fv-plugins-message-container invalid-feedback'>
+                                    {errors.position?.message}
+                                </div>
+                            </div>
+                        </div>
+                        <div className='col-12 gap-5 gap-md-7  mb-6'>
+                            <div className='fv-row flex-row-fluid fv-plugins-icon-container'>
+                                <label className=' form-label'>
+                                    Description
+                                </label>
+                                <textarea
+                                    className='form-control'
+                                    {...register("description")}
+                                    placeholder='description'
+                                />
+                                <div className='fv-plugins-message-container invalid-feedback'>
+                                    {errors.description?.message}
+                                </div>
+                            </div>
+                        </div>
+                        <div className='col-12 d-flex justify-content-end'>
+                            <button
+                                type='submit'
+                                disabled={isLoading}
+                                className='btn btn-primary  mb-6'>
+                                {isLoading ? (
+                                    <>
+                                        <span className='ms-2'>
+                                            Please Wait...
+                                        </span>
+                                        <Spinner
+                                            size='sm'
+                                            animation='border'
+                                            role='status'></Spinner>
+                                    </>
+                                ) : (
+                                    <span>
+                                        {location?.state?.data?.id
+                                            ? "Update"
+                                            : "Submit"}
+                                    </span>
+                                )}
+                            </button>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     );
--- src/ui/shared/components/ServerSelect.tsx
+++ src/ui/shared/components/ServerSelect.tsx
@@ -3,23 +3,25 @@ import { ArrowDown } from "react-bootstrap-icons";
 import useFetch from "../../../hooks/useFetch";
 import { useOnClickOutside } from "usehooks-ts";
 
-interface CommonItem {
-    id: string;
-    name: string;
-    first_name: string;
-}
-interface IProps<T extends CommonItem> {
+interface IProps<T> {
     baseUrl: string;
     selectedListItem: T | undefined;
     setselectedListItem: Dispatch<SetStateAction<T | undefined>>;
     placeholder: string;
-    showDataLabel: string;
+    showDataLabel: keyof T;
+    selectedItemText: string;
+    setSelectedItemText: Dispatch<SetStateAction<string>>;
 }
 
-function ServerSelect<T extends CommonItem>(props: IProps<T>) {
-    const { baseUrl, selectedListItem, setselectedListItem, placeholder } =
-        props;
-    const [selectedItemText, setSelectedItemText] = useState<string>("");
+function ServerSelect<T>(props: IProps<T>) {
+    const {
+        baseUrl,
+        setselectedListItem,
+        placeholder,
+        selectedItemText,
+        setSelectedItemText,
+        showDataLabel,
+    } = props;
     const [currentPage, setCurrentPage] = useState<number>(1);
     const [fetchUrl, setfetchUrl] = useState<string>(
         `${baseUrl}?page=${currentPage}&per_page=15`
@@ -60,7 +62,7 @@ function ServerSelect<T extends CommonItem>(props: IProps<T>) {
         console.log("handleInputClick", item);
         console.log(item);
         setselectedListItem(item);
-        setSelectedItemText(item.first_name);
+        setSelectedItemText(item[showDataLabel] as string);
         setShowListBox(false);
     };
 
@@ -79,16 +81,15 @@ function ServerSelect<T extends CommonItem>(props: IProps<T>) {
     };
 
     useEffect(() => {
-        if (fetchNewData) fetchDataById(fetchUrl);
+        if (fetchNewData && allValue.length === 0) fetchDataById(fetchUrl);
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [fetchNewData]);
 
     useOnClickOutside(selectRef, handleClickOutside);
     return (
-        <div className='server-select'>
+        <div ref={selectRef} className='server-select'>
             <div className='input-group '>
                 <input
-                    ref={selectRef}
                     type='text'
                     value={selectedItemText}
                     onChange={(e) => setSelectedItemText(e.target.value)}
@@ -102,6 +103,7 @@ function ServerSelect<T extends CommonItem>(props: IProps<T>) {
                 </span>
             </div>
             <div
+                onScroll={(e) => handleScroll(e)}
                 className={
                     showListBox
                         ? "list-box-container list-box-show"
@@ -112,7 +114,7 @@ function ServerSelect<T extends CommonItem>(props: IProps<T>) {
                         key={index}
                         onClick={() => handleInputClick(item)}
                         className='server-select-li cursor-pointer'>
-                        {item.first_name}
+                        {item[showDataLabel] as string}
                     </li>
                 ))}
             </div>
