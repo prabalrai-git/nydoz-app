ea89fe907268bd5bdbafb8fdc878d78ec3205a57
rahulprasad0710 <raulshah2021@hotmail.com>
2023-08-30 17:14:48 +0545

    -fix: build

--- src/ui/shared/components/company/CompanyList.tsx
+++ src/ui/shared/components/company/CompanyList.tsx
@@ -4,15 +4,9 @@ import useFetch from "../../../../hooks/useFetch";
 import { ICompanyResponse } from "../../../../types/payload.type";
 import ImageAtom from "../../atoms/ImageAtom";
 import { Link } from "react-router-dom";
-// import useWebSetting from "../../../../context/useWebSetting";
 import DynamicLink from "../../molecules/DynamicLink";
-// import AppSetting from "../../../../config/AppSetting";
 
 const CompanyListCard = () => {
-    // const { VITE_HOST } = AppSetting;
-    // const navigate = useNavigate();
-    // const { hasSubdomain } = useWebSetting();
-
     const { data, fetchData, isloading } = useFetch<ICompanyResponse[]>(
         API_ROUTE.GET_COMPANIES,
         true
@@ -23,10 +17,6 @@ const CompanyListCard = () => {
 
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
-
-    // const handleNavigateToSubDomain = (subdomain: string) => {
-    //     navigate(`${subdomain}/dashboard`);
-    // };
 
     return (
         <div className='card h-xl-100'>
