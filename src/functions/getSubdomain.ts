7de1fdb3489e549cf48b1c2fd3fb44597061dfad
rahulprasad0710 <raulshah2021@hotmail.com>
2023-09-01 14:17:56 +0545

    wip: prod env

--- src/functions/getSubdomain.ts
+++ src/functions/getSubdomain.ts
@@ -9,6 +9,8 @@ const getBaseUrl = (url: string) => {
 export const getSubdomain = () => {
     const baseUrl = getBaseUrl(window.location.href);
     console.log("url from browser", baseUrl);
+    console.log({ DOMAIN, ENVIRONMENT, baseUrl });
+
     const [_httpPartMain, mainDomainPart] = DOMAIN.split("://");
 
     if (ENVIRONMENT === "development") {
